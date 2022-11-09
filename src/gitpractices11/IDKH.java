68043d50d680194f0575f0923f13f9a142f7f40f
Sergio Palomino Guevara <sergiopalominoguevara@sergios-mbp.mynetworksettings.com>
2022-11-08 20:51:07 -0500

    update again

--- src/gitpractices11/IDKH.java
+++ src/gitpractices11/IDKH.java
@@ -1,5 +1,30 @@
 package gitpractices11;
 
 public class IDKH {
+	
 
-}
+	
+		public static void main(String[] args) {
+			
+			System.out.println("this is atest");
+		
+		
+		
+		
+		
+		
+
+		
+		}
+
+	public void test() {
+		System.out.println("this is gfdfatest");
+	}
+
+	public void test11() {
+		System.out.println("this isdgsgadgafhafs gfdfatest");
+	
+	}
+	}
+
+
